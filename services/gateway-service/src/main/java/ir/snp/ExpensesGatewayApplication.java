25eaea4c5aa5df6e554d0dac76a88338f595b70e
moeini <youremail@example.com>
2024-12-31 11:16:08 +0330

    initialized gateway application as resource server

---
+++ services/gateway-service/src/main/java/ir/snp/ExpensesGatewayApplication.java
@@ -0,0 +1,11 @@
+package ir.snp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensesGatewayApplication.class);
+    }
+}
