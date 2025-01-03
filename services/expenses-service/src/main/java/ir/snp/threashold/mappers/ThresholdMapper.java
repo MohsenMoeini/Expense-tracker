3c801545dbc6a3fee8b987a64ebd1c356f57b097
moeini <moeini.mhsn@gmail.com>
2025-01-04 00:06:37 +0330

    fix: use the stored thresholds instead of calculating them from expenses

--- services/expenses-service/src/main/java/ir/snp/threashold/mappers/ThresholdMapper.java
+++ services/expenses-service/src/main/java/ir/snp/threashold/mappers/ThresholdMapper.java
@@ -2,8 +2,6 @@ package ir.snp.threashold.mappers;
 
 import ir.snp.expense.dto.MoneyDTO;
 import ir.snp.expense.entity.Money;
-import ir.snp.threashold.dto.ThresholdResponseDTO;
-import ir.snp.threashold.entity.ExpenseThreshold;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
